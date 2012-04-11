1768fc0583243c99b2a0e48b47b5eb31cc047dc1
Tony Arcieri <tony.arcieri@gmail.com>
2012-04-10 19:03:58 -0700

    Start NodeManager before Server

--- lib/dcell.rb
+++ lib/dcell.rb
@@ -104,9 +104,9 @@ module DCell
 
   # DCell's actor dependencies
   class Group < Celluloid::Group
+    supervise NodeManager, :as => :node_manager
     supervise Server,      :as => :dcell_server
     supervise InfoService, :as => :info
-    supervise NodeManager, :as => :node_manager
   end
 
   Logger = Celluloid::Logger
